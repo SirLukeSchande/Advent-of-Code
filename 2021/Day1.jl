9b26366bf0b81e8a72cebef4e8c4ba3f982bca42
Lukas Schneider <lukasschn@gmail.com>
2021-12-07 17:22:54 +0000

    completed Day 1

---
+++ 2021/Day1.jl
@@ -0,0 +1,36 @@
+# --------------------------------------------------------------------#
+#                               Part 1                               #
+# --------------------------------------------------------------------#
+
+function larger_measurements()
+    sonar_input = map(l -> parse(Int, l), readlines("input1.dat"))
+
+    return count(diff(sonar_input) .> 0)
+end
+
+println("$(larger_measurements()) measurements are larger than the one before.")
+
+
+# --------------------------------------------------------------------#
+#                               Part 2                               #
+# --------------------------------------------------------------------#
+
+function walking_window(func::Function, x::AbstractVector, n::Int)
+    res = Vector{Number}(undef, length(x) - n + 1)
+
+    for i in eachindex(res)
+        res[i] = func(x[i:(i + n - 1)])
+    end
+
+    return res
+end
+
+function larger_sum_measurements()
+    sonar_input = map(l -> parse(Int, l), readlines("input1.dat"))
+
+    sonar_sum3 = walking_window(sum, sonar_input, 3)
+
+    return count(diff(sonar_sum3) .> 0)
+end
+
+println("$(larger_sum_measurements()) sums of measurements are larger than the sum before.")
--- 2021/Day1_1.jl
+++
@@ -1,7 +0,0 @@
-function larger_measurements()
-    sonar_input = map(l -> parse(Int, l), readlines("input1.dat"))
-
-    return count(diff(sonar_input) .> 0)
-end
-
-larger_measurements()
