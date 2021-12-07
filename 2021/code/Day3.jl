eadcf20bf25f574d079f3bfbd9e5e6a19977c89d
Lukas Schneider <lukasschn@gmail.com>
2021-12-07 21:19:32 +0000

    completed Day 3

--- 2021/code/Day3.jl
+++ 2021/code/Day3.jl
@@ -34,3 +34,34 @@ println("The gamma rate is $gamma and the epsilon rate is $epsilon.\nSo the answ
 #                               Part 2                               #
 # --------------------------------------------------------------------#
 
+
+function reduced_diagnostic(most_common::Bool)
+    col = 1
+    res = read_matrix("input/input3.dat")
+
+    while size(res, 1) > 1
+        if most_common
+            res = res[res[:,col] .== most_common_bit(res[:,col]),:]
+        else
+            res = res[res[:,col] .!= most_common_bit(res[:,col]),:]
+        end
+        col = mod(col, size(res, 2)) + 1
+    end
+    
+    return res
+end
+
+
+function ratings()
+    oxygen_generator_bin = reduced_diagnostic(true)
+    co2_scrubber_bin = reduced_diagnostic(false)
+
+    oxygen_generator_rating = parse(Int, join(string.(oxygen_generator_bin)), base=2)
+    co2_scrubber_rating = parse(Int, join(string.(co2_scrubber_bin)), base=2)
+
+    return oxygen_generator_rating, co2_scrubber_rating
+end
+
+oxygen_generator_rating, co2_scrubber_rating = ratings()
+println("The oxygen generator rating and the co2 scrubber rating are $oxygen_generator_rating and $co2_scrubber_rating, respectively.")
+println("The answer to part two is $(oxygen_generator_rating * co2_scrubber_rating)")
